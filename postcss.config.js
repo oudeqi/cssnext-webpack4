8296699f276caa44561f9353a9e95a2ffd13a1a5
ouwei <496997374@qq.com>
2018-04-09 18:16:17 +0800

    postcss-px2rem不转换1px

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,10 @@ module.exports = function({file, options, env}){
 	return {
 		plugins: {
 			'postcss-import': {},
-			'postcss-cssnext': {}
+			'postcss-cssnext': {},
+			'postcss-px2rem': {
+				remUnit: 14 ////px转rem，如果不需要转换，在样式规则后面加：/*no*/
+			}
 		}
 	}
 }
